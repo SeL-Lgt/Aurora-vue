f9d87ded578046dd461e32fe784668c4d6e86ea9
SeL-LGT <122790345@qq.com>
2020-07-14 22:23:14 +0800

    登录界面重置（表单样式未修改）

--- src/views/common/login.vue
+++ src/views/common/login.vue
@@ -1,37 +1,30 @@
 <template>
-  <div>
-    <div id="login">
+  <div id="login">
+    <div class="sky" ref="sky">
+      <canvas ref="canvas"></canvas>
+      <el-card class="myCard" shadow="always">
+        <div slot="header">
+          <h1 style="text-align: center">Aurora</h1>
+        </div>
+        <el-form :model="form" :rules="form" :ref="form">
+          <el-form-item prop="account">
+            <el-input v-model="form.account">
+              <template slot="prepend"><i class="el-icon-s-custom"></i></template>
+            </el-input>
+          </el-form-item>
+          <el-form-item prop="password">
+            <el-input v-model="form.password" type="password">
+              <template slot="prepend"><i class="el-icon-postcard"></i></template>
+            </el-input>
+          </el-form-item>
+          <el-form-item>
+            <el-button type="primary" @click="login">登录</el-button>
+            <el-button @click="resetForm(form)">重置</el-button>
+          </el-form-item>
+        </el-form>
+        <el-link type="danger" :underline="false" @click="$router.push({name:'registered'})">没有账号?注册一下</el-link>
+      </el-card>
     </div>
-    <el-card class="myCard" shadow="always">
-      <div slot="header">
-        <h1 style="text-align: center">Aurora</h1>
-      </div>
-      <el-form :model="form" :rules="form" :ref="form">
-        <el-form-item prop="account">
-          <el-input v-model="form.account">
-            <template slot="prepend"><i class="el-icon-s-custom"></i></template>
-          </el-input>
-        </el-form-item>
-        <el-form-item prop="password">
-          <el-input v-model="form.password" type="password">
-            <template slot="prepend"><i class="el-icon-postcard"></i></template>
-          </el-input>
-        </el-form-item>
-        <el-form-item prop="authority">
-          <el-radio-group v-model="form.authority">
-            <el-radio label="1">用户</el-radio>
-            <el-radio label="2">供应商</el-radio>
-            <el-radio label="3">管理员</el-radio>
-          </el-radio-group>
-        </el-form-item>
-        <el-form-item>
-          <el-button type="primary" @click="login">登录</el-button>
-          <el-button @click="resetForm(form)">重置</el-button>
-        </el-form-item>
-      </el-form>
-      <el-link type="danger" :underline="false" @click="$router.push({name:'registered'})">没有账号?注册一下</el-link>
-    </el-card>
-
   </div>
 </template>
 
@@ -44,132 +37,129 @@
           account: null,
           password: null,
           authority: null
-        }
+        },
+        starsCount: 800,//星星数量
+        distance: 800,//间隔
       }
     },
-    methods: {
-      /**
-       * 重置查询
-       * @param formName
-       */
-      resetForm(formName) {
-        this.$refs[formName].resetFields();
-      },
-      /**
-       * 登录
-       */
-      login() {
-        console.log(this.form);
-        this.$api.axiosPost("/admin/login/", {
-          account: this.form.account,
-          password: this.form.password,
-          authority: this.form.authority
-        }).then(res => {
-          if (res.code == 200) {
-            this.$message({message: "登录成功", type: "success"});
-            this.$router.push({name: 'home'});
-          } else
-            this.$message.error('登录失败');
-        })
+    mounted() {
+      let _this = this;
+      _this.$refs.sky.width = window.innerWidth;
+      _this.$refs.sky.height = window.innerHeight;
+
+      var canvas = _this.$refs.canvas,
+          ctx = canvas.getContext("2d"),
+          _width = _this.$refs.sky.width,
+          _height = _this.$refs.sky.height,
+          stars = [],
+          initStarsPopulation = 300;
+
+
+      function Star(id, x, y) {
+        this.id = id;
+        this.x = x;
+        this.y = y;
+        this.r = Math.floor(Math.random() * 2) + 1;
+        const alpha = (Math.floor(Math.random() * 10) + 1) / 10 / 2;
+        this.color = "rgba(255,255,255," + alpha + ")"
       }
-    }
+
+      Star.prototype.draw = function () {
+        ctx.fillStyle = this.color;
+        ctx.shadowBlur = this.r * 2;
+        ctx.beginPath();
+        ctx.arc(this.x, this.y, this.r, 0, 2 * Math.PI, false);
+        ctx.closePath();
+        ctx.fill();
+      }
+      Star.prototype.move = function () {
+        this.y -= 1;
+        if (this.y <= -10) this.y = _height + 10;
+        this.draw();
+      }
+      Star.prototype.die = function () {
+        stars[this.id] = null;
+        delete stars[this.id];
+      }
+
+      function setCanvasSize() {
+        canvas.setAttribute("width", _width)
+        canvas.setAttribute("height", _height)
+      }
+
+      function _init_() {
+        ctx.strokeStyle = "white";
+        ctx.shadowColor = "white";
+        for (var i = 0; i < initStarsPopulation; i++) {
+          stars[i] = new Star(i, Math.floor(Math.random() * _width), Math.floor(Math.random() * _height));
+        }
+        ctx.shadowBlur = 0;
+        animate();
+      }
+
+      setCanvasSize();
+      _init_();
+
+      function animate() {
+        ctx.clearRect(0, 0, _width, _height);
+        for (var i in stars) {
+          stars[i].move();
+        }
+        requestAnimationFrame(animate);
+      }
+    },
+    methods: {}
   }
 </script>
 
 <style scoped>
-  #login {
-    position: relative;
-    background: url("../../assets/images/wallhaven-q6yd5q.jpg") no-repeat center center fixed;
-    filter: brightness(.5);
-    background-size: 100%;
+  .sky {
+    background: radial-gradient(220% 105% at top center, #1b2947 10%, #75517d 40%, #e96f92 65%, #f7f7b8);
     height: 100vh;
-    transition: filter;
-    transition-duration: 0.8s;
-    transition-timing-function: ease-in-out;
+    overflow: hidden;
   }
 
-  #login:hover {
-    filter: brightness(1);
+  .stars {
+    transform: perspective(300px);
+    transform-style: preserve-3d;
+    position: absolute;
+    perspective-origin: 50% 100%;
+    left: 0%;
+    bottom: 0;
+    animation: rotate 90s infinite linear;
   }
+
+  .star {
+    width: 2px;
+    height: 2px;
+    background: #f7f7b8;
+    position: absolute;
+    top: 0;
+    left: 0;
+    backface-visibility: hidden;
+  }
+
 
   .myCard {
     position: absolute;
     left: 50%;
     top: 50%;
     transform: translateX(-50%) translateY(-50%);
+    width: 25%;
+    background-color: rgba(255, 255, 255, .3);
+    border: 2px solid white;
+    box-shadow: 20px 20px 20px rgba(0, 0, 0, .5);
+    border-radius: 20px;
     z-index: 100;
   }
 
-  .el-form-item {
-    text-align: center;
-  }
-
-  .el-form >>> .el-input {
-    width: 90%;
-  }
-
-  .el-card.is-always-shadow, .el-card.is-hover-shadow:focus, .el-card.is-hover-shadow:hover {
-    box-shadow: 0 2px 20px 0 rgba(5, 119, 220, 0.92);
-  }
-
-  .el-card {
-    width: 25%;
-    background-color: #fff;
-    transition: background-color;
-    transition-duration: 0.8s;
-    transition-timing-function: ease-in-out;
-  }
-
-  .el-card >>> .el-input__inner,
-  .el-card >>> .el-input-group__prepend {
-    border: 1px solid rgba(177, 172, 172, 0.53);
-    transition: border;
-    transition-duration: 0.8s;
-    transition-timing-function: ease-in-out;
-  }
-
-  .el-card:hover {
-    background: rgba(255, 255, 255, 0.75);
-  }
-
-  .el-card:hover >>> .el-input__inner {
-    transition: font-weight, color;
-    transition-duration: 0.8s;
-    transition-timing-function: ease-in-out;
-    color: #151212;
-    /*font-weight: 700;*/
-    background: #fff9;
-    border: 1px solid #dcdfe6;
-  }
-
-  .el-card:hover >>> .el-radio {
-    color: rgba(0, 0, 0, 0.8);
-  }
-
-  .el-card >>> .el-card__header {
-    background: rgba(5, 119, 220, 0.92);
-    color: #fff;
-    text-shadow: #fff;
-  }
-
-  .el-card:hover >>> .el-form-item__label {
-    transition: color;
-    transition-duration: 0.8s;
-    transition-timing-function: ease-in-out;
-    color: #151212;
-  }
-
-  .el-card:hover >>> .el-link.el-link--danger {
-    transition: font-weight;
-    transition-duration: 0.8s;
-    transition-timing-function: ease-in-out;
-    font-weight: 700;
-    color: #e01212;
-  }
-
-  .el-select {
-    width: 100%;
-    margin-left: 5%;
+  @keyframes rotate {
+    0% {
+      transform: perspective(400px) rotateZ(20deg) rotateX(-40deg) rotateY(0);
+    }
+    100% {
+      transform: perspective(400px) rotateZ(20deg) rotateX(-40deg) rotateY(360deg);
+    }
   }
 
 </style>
