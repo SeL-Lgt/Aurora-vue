390d44640331fc21fe28e815d6e2d036e297c9f4
SeL-LGT <122790345@qq.com>
2020-07-27 21:54:33 +0800

    功能二穿梭框前后端未交互 新增模板、编辑未做

--- src/views/pages/Configuration/configurationTemplate.vue
+++ src/views/pages/Configuration/configurationTemplate.vue
@@ -3,7 +3,7 @@
     <el-row>
       <el-col>
         <el-button type="primary">新增模板<i class="el-icon-upload el-icon--right"></i></el-button>
-        <el-button type="primary" @click="dialogTableVisible=true">批量配置<i class="el-icon-upload el-icon--right"></i>
+        <el-button type="primary" @click="configurationTemplate">批量配置<i class="el-icon-upload el-icon--right"></i>
         </el-button>
       </el-col>
     </el-row>
@@ -25,37 +25,49 @@
     <el-dialog title="批量配置模板" :visible.sync="dialogTableVisible" width="80%">
       <el-row :gutter="20">
         <el-col :span="10">
-          <el-table :data="modelCard">
+          <p>可选择模板</p>
+          <el-table :data="leftData" ref="leftTemp" @selection-change="checkLeft" :default-sort="{prop:'id'}"
+                    max-height="250">
             <el-table-column type="selection"></el-table-column>
-            <el-table-column property="id" label="ID"></el-table-column>
-            <el-table-column property="modelname" label="模板名字"></el-table-column>
-            <el-table-column property="remakes" label="接口名字"></el-table-column>
-            <el-table-column property="introduce" label="地址"></el-table-column>
+            <el-table-column prop="id" label="ID"></el-table-column>
+            <el-table-column prop="modelname" label="模板名字"></el-table-column>
+            <el-table-column prop="remakes" label="接口名字"></el-table-column>
+            <el-table-column prop="introduce" label="地址"></el-table-column>
           </el-table>
         </el-col>
-        <el-col :span="4" style="text-align: center;position: relative;top:0%;transform: translateY(50%)">
+        <el-col :span="4" style="text-align: center;position: relative;top:50%;transform: translateY(50%)">
           <el-row>
             <el-button icon="el-icon-d-arrow-right"
-                       type="primary">
+                       type="primary"
+                       @click="addConfigurationTemplate"
+                       :disabled="leftData.length?(leftTemp.length?false:true):true"
+            >
               添加
             </el-button>
           </el-row>
           <el-row>
             <el-button icon="el-icon-d-arrow-left"
-                       type="primary">
+                       type="primary"
+                       @click="deleteConfigurationTemplate"
+                       :disabled="rightData.length?(rightData.length?false:true):true"
+            >
               移除
             </el-button>
           </el-row>
         </el-col>
         <el-col :span="10">
-          <el-table :data="modelCard">
+          <p>已选择模板（并按此顺序配置）</p>
+          <el-table :data="rightData" ref="rightTemp" @selection-change="checkRight" max-height="250">
             <el-table-column type="selection"></el-table-column>
-            <el-table-column property="id" label="ID"></el-table-column>
-            <el-table-column property="modelname" label="模板名字"></el-table-column>
-            <el-table-column property="remakes" label="接口名字"></el-table-column>
-            <el-table-column property="introduce" label="地址"></el-table-column>
+            <el-table-column prop="id" label="ID"></el-table-column>
+            <el-table-column prop="modelname" label="模板名字"></el-table-column>
+            <el-table-column prop="remakes" label="接口名字"></el-table-column>
+            <el-table-column prop="introduce" label="地址"></el-table-column>
           </el-table>
         </el-col>
+      </el-row>
+      <el-row style="text-align: center">
+        <el-button type="primary" :disabled="rightData.length?false:true">确定配置</el-button>
       </el-row>
     </el-dialog>
   </div>
@@ -77,14 +89,13 @@
             "modelname": "LoopBack6",
             "remakes": "创汇还口",
             "introduce": "回环1"
-          },
-          {
-            "id": 2,
+          }, {
+            "id": 6,
             "modelname": "LoopBack6",
             "remakes": "创汇还口",
-            "introduce": "回环2"
+            "introduce": "回环1"
           }, {
-            "id": 1,
+            "id": 7,
             "modelname": "LoopBack6",
             "remakes": "创汇还口",
             "introduce": "回环1"
@@ -94,14 +105,78 @@
             "modelname": "LoopBack6",
             "remakes": "创汇还口",
             "introduce": "回环2"
+          }, {
+            "id": 3,
+            "modelname": "LoopBack6",
+            "remakes": "创汇还口",
+            "introduce": "回环1"
+          },
+          {
+            "id": 4,
+            "modelname": "LoopBack6",
+            "remakes": "创汇还口",
+            "introduce": "回环2"
           }
-        ]
+        ],
+        rightData: [],
+        leftData: [],
+        rightTemp: [],
+        leftTemp: []
       }
     },
-    methods:{
+    methods: {
       /**
        * 批量配置模板
        */
+      configurationTemplate() {
+        this.dialogTableVisible = true;
+        this.leftData = JSON.parse(JSON.stringify(this.modelCard));
+        this.rightData = []
+        console.log(this.leftData);
+      },
+      /**
+       * 选择暂存数组
+       */
+      checkRight(val) {
+        this.rightTemp = val;
+      },
+      checkLeft(val) {
+        this.leftTemp = val;
+      },
+      /**
+       * 添加到预安装模板
+       */
+      addConfigurationTemplate() {
+        this.rightData = this.handleConcatArr(this.rightData, this.leftTemp);
+        this.handleRemoveTabList(this.leftTemp, this.leftData);
+        this.leftTemp = [];
+      },
+      /**
+       * 移除
+       */
+      deleteConfigurationTemplate() {
+        this.leftData = this.handleConcatArr(this.leftData, this.rightTemp);
+        this.handleRemoveTabList(this.rightTemp, this.rightData);
+        this.rightTemp = [];
+        this.leftData.sort();
+      },
+
+      handleConcatArr(a, b) {
+        let arr = [];
+        arr = arr.concat(a, b);
+        return arr;
+      },
+      handleRemoveTabList(isNeedArr, originalArr) {
+        if (isNeedArr.length && originalArr.length) {
+          for (let i = 0; i < isNeedArr.length; i++) {
+            for (let k = 0; k < originalArr.length; k++) {
+              if (isNeedArr[i]["id"] === originalArr[k]["id"]) {
+                originalArr.splice(k, 1);
+              }
+            }
+          }
+        }
+      }
     }
   }
 </script>
@@ -126,5 +201,4 @@
   .card-Content {
     text-align: center;
   }
-
 </style>
